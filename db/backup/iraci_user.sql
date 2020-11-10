6840942f4fe2dc0aa0df3078e046007ce9a99051
Cuchini <davide.iraci@community.unipa.it>
2020-11-10 12:57:01 +0100

    db back

--- db/backup/iraci_user.sql
+++ db/backup/iraci_user.sql
@@ -30,11 +30,11 @@ CREATE TABLE `user` (
   `email` varchar(100) NOT NULL,
   `role` varchar(45) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `telephone` varchar(13) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `telephone` varchar(10) NOT NULL,
   PRIMARY KEY (`id_User`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `telephone_UNIQUE` (`telephone`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Davide','Iraci','1997-05-15','iraci.davide@gmail.com','Admin','c8a2035c91b566a5dffc34d586408a298abf95ca3d706ead1c677b4979a44c57','3297788345'),(2,'Marco','La Martina','1997-05-28','marcolamartina@hotmail.it','User','7c8ccc86c11654af029457d90fdd9d013ce6fb011ee8fdb1374832268cc8d967','3272294014');
+INSERT INTO `user` VALUES (1,'Davide','Iraci','1997-05-15','iraci.davide@gmail.com','Admin','606e899c8cfd8285a9fe73f55a36d0f2d854e160fe9129cbb8ec897c35edcef9','3297788345','0916261873'),(2,'Marco','La Martina','1997-05-28','marcolamartina@hotmail.it','Bagnino','f3c6a505e2f2555fa36b7a78b1777eca83896eb2b26bf89c0927f80425bb5801','3272294014',''),(3,'Roberto','Manzo','1997-02-21','roberto.manzo01@community.unipa.it','Cucina','5234fd45e6d67fe0a151cc25d8304623603a82d5f2b9f70daf46d7478d5013fc','3392063066',''),(4,'Andrea','Montemaggiore','1997-08-26','andrea.montemaggiore01@community.unipa.it','User','3bb84cf266615e9f2db89ed1ef89ca764ec2afa51a9a7804500c2b7e504bcfe8','3279863539',''),(5,'Giovanni','Raccuglia','1996-12-24','giovy9674@gmail.com','Banco','03c809b539aa81f1c45b4ce1ddd0c355cfdc60a9f6bfb6e1eef17c3cb1c06b47','3273098779','');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-18 20:06:36
+-- Dump completed on 2020-11-10 12:54:58
